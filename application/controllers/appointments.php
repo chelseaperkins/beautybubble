14df4f02298e315fbee71fcaa51c9aa790348cf5
chelseaperkins <chelsea@perkins.org.nz>
2015-04-13 14:15:03 +1200

    css styling

--- application/controllers/appointments.php
+++ application/controllers/appointments.php
@@ -4,9 +4,26 @@ if (!defined('BASEPATH'))
     exit('No direct script access allowed');
 
 class Appointments extends CI_Controller {
+
     //put your code here
-    	public function index()
-	{
-		echo 'Hello Appointments!';
-	}
+    public function index() {
+        echo 'Hello Appointments!';
+    }
+
+    public function email() {
+        $this->load->library('email');
+
+        $this->email->from('your@example.com', 'Your Name');
+        $this->email->to('someone@example.com');
+        $this->email->cc('another@another-example.com');
+        $this->email->bcc('them@their-example.com');
+
+        $this->email->subject('Email Test');
+        $this->email->message('Testing the email class.');
+
+        $this->email->send();
+
+        echo $this->email->print_debugger();
+    }
+
 }
